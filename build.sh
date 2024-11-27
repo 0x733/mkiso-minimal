7e3407eb663ea62b9c111c92afef792bef30c602
0x733 <98838267+0x733@users.noreply.github.com>
2024-11-27 04:43:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,141 +1,74 @@
 #!/bin/bash
 set -e
-#### Check root
-if [[ ! $UID -eq 0 ]] ; then
-    echo -e "\033[31;1mYou must be root!\033[:0m"
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You must be root to run this script."
     exit 1
 fi
-#### Remove all environmental variable
-for e in $(env | sed "s/=.*//g") ; do
+
+for e in $(env | sed "s/=.*//g"); do
     unset "$e" &>/dev/null
 done
 
-#### Set environmental variables
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 export LANG=C
 export SHELL=/bin/bash
 export TERM=linux
 export DEBIAN_FRONTEND=noninteractive
 
-#### Install dependencies
-if which apt &>/dev/null && [[ -d /var/lib/dpkg && -d /etc/apt ]] ; then
-    apt-get update
-    apt-get install curl mtools squashfs-tools grub-pc-bin grub-efi-amd64-bin grub2-common grub-common grub-efi-ia32-bin xorriso debootstrap binutils -y --no-install-recommends
-fi
+apt-get update
+apt-get install -y curl mtools squashfs-tools grub-pc-bin grub-efi-amd64-bin \
+    grub2-common xorriso debootstrap binutils --no-install-recommends
 
-set -ex
-#### Chroot create
-mkdir chroot || true
+mkdir -p chroot
 
-#### For devuan
-debootstrap --variant=minbase --no-check-gpg --no-merged-usr --exclude=usrmerge --arch=amd64 testing chroot https://pkgmaster.devuan.org/merged
-echo "deb https://pkgmaster.devuan.org/merged testing main contrib non-free non-free-firmware" > chroot/etc/apt/sources.list
+debootstrap --variant=minbase --no-check-gpg --arch=amd64 bullseye chroot http://deb.debian.org/debian
+echo "deb http://deb.debian.org/debian bullseye main contrib non-free non-free-firmware" > chroot/etc/apt/sources.list
 
-#### Set root password
-pass="live"
-echo -e "$pass\n$pass\n" | chroot chroot passwd
+echo -e "live\nlive\n" | chroot chroot passwd
 
-#### Fix apt & bind
-# apt sandbox user root
 echo "APT::Sandbox::User root;" > chroot/etc/apt/apt.conf.d/99sandboxroot
-for i in dev dev/pts proc sys; do mount -o bind /$i chroot/$i; done
-chroot chroot apt-get install gnupg --no-install-recommends  -y
+for dir in dev dev/pts proc sys; do mount --bind /$dir chroot/$dir; done
 
-##### Devuan only
-chroot chroot apt-get install devuan-keyring --no-install-recommends -y
+chroot chroot apt-get install -y gnupg network-manager live-config live-boot --no-install-recommends
 
-#### Debjaro repository (optional)
-echo "deb https://debjaro.github.io/repo/stable stable main" > chroot/etc/apt/sources.list.d/debjaro.list
-curl https://debjaro.github.io/repo/stable/dists/stable/Release.key | gpg --dearmor > chroot/etc/apt/trusted.gpg.d/debjaro.gpg
-chroot chroot apt-get update -y
-chroot chroot apt-get full-upgrade -y
-
-
-#### live packages for debian/devuan
-chroot chroot apt-get install live-config live-boot network-manager --no-install-recommends  -y
-echo "DISABLE_DM_VERITY=true" >> chroot/etc/live/boot.conf
-
-#### Configure system
-cat > chroot/etc/apt/apt.conf.d/01norecommend << EOF
-APT::Install-Recommends "0";
-APT::Install-Suggests "0";
-EOF
-
-# Set sh as bash inside of dash (optional)
-rm -f chroot/bin/sh
-ln -s bash chroot/bin/sh
-
-#### Remove bloat files after dpkg invoke (optional)
-cat > chroot/etc/apt/apt.conf.d/02antibloat << EOF
-DPkg::Post-Invoke {"rm -rf /usr/share/locale || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/man || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/help || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/doc || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/info || true";};
-DPkg::Post-Invoke {"rm -rf /usr/share/i18n || true";};
-EOF
-
-
-#### liquorix kernel
+echo "deb http://liquorix.net/debian bullseye main" > chroot/etc/apt/sources.list.d/liquorix.list
 curl https://liquorix.net/liquorix-keyring.gpg | chroot chroot apt-key add -
-echo "deb http://liquorix.net/debian testing main" > chroot/etc/apt/sources.list.d/liquorix.list
-chroot chroot apt-get update -y
-chroot chroot apt-get install linux-image-liquorix-amd64 -y
-#chroot chroot apt-get install linux-headers-liquorix-amd64 -y
+chroot chroot apt-get update
+chroot chroot apt-get install -y linux-image-liquorix-amd64 linux-headers-liquorix-amd64
 
-#### usbcore stuff (for initramfs)
-echo "#!/bin/sh" > chroot/etc/initramfs-tools/scripts/init-top/usbcore.sh
-echo "echo Y > /sys/module/usbcore/parameters/old_scheme_first" >> chroot/etc/initramfs-tools/scripts/init-top/usbcore.sh
-chmod +x chroot/etc/initramfs-tools/scripts/init-top/usbcore.sh
+chroot chroot apt-get install -y task-gnome-desktop gnome-terminal gnome-tweaks
 
-### remove unused modules (optional)
-rm -rf  chroot/lib/modules/*/kernel/drivers/media
-rm -rf  chroot/lib/modules/*/kernel/drivers/gpu
-rm -rf  chroot/lib/modules/*/kernel/sound
-chroot chroot depmod -a $(ls chroot/lib/modules)
+fallocate -l 8G chroot/swapfile
+chmod 600 chroot/swapfile
+mkswap chroot/swapfile
+echo "/swapfile none swap sw 0 0" >> chroot/etc/fstab
+
 chroot chroot update-initramfs -u -k all
 
-### Remove sudo (optional)
-chroot chroot apt purge sudo -y
-chroot chroot apt autoremove --purge -y
+rm -f chroot/etc/apt/apt.conf.d/01norecommend
 
-#### Clear logs and history
 chroot chroot apt-get clean
-rm -f chroot/root/.bash_history
 rm -rf chroot/var/lib/apt/lists/*
-find chroot/var/log/ -type f | xargs rm -f
+find chroot/var/log/ -type f -exec rm -f {} \;
 
-### Create iso template
-mkdir -p debjaro/boot || true
-mkdir -p debjaro/live || true
-ln -s live debjaro/casper || true
+mkdir -p iso/boot iso/live
+cp -pf chroot/boot/vmlinuz-* iso/boot/vmlinuz
+cp -pf chroot/boot/initrd.img-* iso/boot/initrd.img
 
-#### Copy kernel and initramfs (Debian/Devuan)
-cp -pf chroot/boot/initrd.img-* debjaro/boot/initrd.img
-cp -pf chroot/boot/vmlinuz-* debjaro/boot/vmlinuz
+for dir in dev dev/pts proc sys; do umount -lf chroot/$dir; done
+mksquashfs chroot iso/live/filesystem.squashfs -comp xz -wildcards
 
-#### remove vmlinuz and initrd for minimize iso size (optional)
-rm -rf chroot/boot/initrd.img-*
-rm -rf chroot/boot/vmlinuz-*
+mkdir -p iso/boot/grub/
+cat > iso/boot/grub/grub.cfg << EOF
+menuentry "Start GNOME Debian GNU/Linux with Liquorix Kernel" {
+    linux /boot/vmlinuz boot=live quiet
+    initrd /boot/initrd.img
+}
+EOF
 
-#### Create squashfs
-for dir in dev dev/pts proc sys ; do
-    while umount -lf -R chroot/$dir 2>/dev/null ; do true; done
-done
-# For better installation time
-#mksquashfs chroot filesystem.squashfs -comp gzip -wildcards
-# For better compress ratio
-mksquashfs chroot filesystem.squashfs -comp xz -wildcards
+ISO_NAME="gnome-liquorix-debian-$(date +%Y%m%d).iso"
+grub-mkrescue iso -o "$ISO_NAME"
 
-### Move squashfs file into iso template
-mv filesystem.squashfs debjaro/live/filesystem.squashfs
-
-#### Write grub.cfg
-mkdir -p debjaro/boot/grub/
-echo 'menuentry "Start Debjaro GNU/Linux 64-bit" --class debjaro {' > debjaro/boot/grub/grub.cfg
-echo '    linux /boot/vmlinuz boot=live live-config quiet --' >> debjaro/boot/grub/grub.cfg
-echo '    initrd /boot/initrd.img' >> debjaro/boot/grub/grub.cfg
-echo '}' >> debjaro/boot/grub/grub.cfg
-
-#### Create iso
-grub-mkrescue debjaro -o debjaro-gnulinux-$(date +%s).iso
+rm -rf chroot iso
+echo "GNOME Liquorix Kernel Debian ISO created: $ISO_NAME"
